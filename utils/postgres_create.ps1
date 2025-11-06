ed43acb2d321222c696c2a1df39d0b0f24db6229
Artymka <doomparser@gmail.com>
2025-11-07 01:32:34 +0300

    add: tools for work with postgres container in docker

---
+++ utils/postgres_create.ps1
@@ -0,0 +1,3 @@
+docker run --name iskra-postgres -p 5432:5432 -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -d postgres:13.22-alpine3.22
+# docker start iskra-postgres
+# psql -U postgres -d postgres -p 5432
---
+++ utils/postgres_delete.ps1
@@ -0,0 +1,2 @@
+docker stop iskra-postgres
+docker rm iskra-postgres
