d7a43ddb6edc925899fafeea1fd6e88bc42c5c92
Чудаев Иван <dreyk.o@mail.ru>
2025-11-07 16:28:18 +0300

    Sexuallnyo separeted templates, loooks good

--- miniapp/internal/tools/renderTemplate.go
+++ miniapp/internal/tools/renderTemplate.go
@@ -8,14 +8,28 @@ import (
 )
 
 func RenderTemplate(w http.ResponseWriter, cfg *config.Config, tmpl string, data interface{}) error {
-	templates := template.Must(template.ParseFiles(
-		//"internal/templates/base.html",
-		cfg.MiniApp.TemplatesPath + tmpl,
-	))
+	var templates *template.Template
+	if tmpl != "start.html" {
+		templates = template.Must(template.ParseFiles(
+			cfg.MiniApp.TemplatesPath+"base.html",
+			cfg.MiniApp.TemplatesPath+tmpl,
+		))
+	} else {
+		templates = template.Must(template.ParseFiles(
+			cfg.MiniApp.TemplatesPath + tmpl,
+		))
+	}
 
-	err := templates.ExecuteTemplate(w, tmpl, data)
-	if err != nil {
-		return fmt.Errorf("error with execute template: %s", err)
+	if tmpl != "start.html" {
+		err := templates.ExecuteTemplate(w, "base.html", data)
+		if err != nil {
+			return fmt.Errorf("error with execute template: %s", err)
+		}
+	} else {
+		err := templates.ExecuteTemplate(w, "start.html", data)
+		if err != nil {
+			return fmt.Errorf("error with execute template: %s", err)
+		}
 	}
 	return nil
 }
--- shared/storage/postgres/postgres_test.go
+++ shared/storage/postgres/postgres_test.go
@@ -17,17 +17,18 @@ func TestUserRepo(t *testing.T) {
 
 	t.Run("Basic test", func(t *testing.T) {
 		err := s.UserRepo.CreateUser(models.UserCreate{
-			ChatID:      123,
-			Username:    "abc",
-			Nick:        "abc",
-			Description: "cba",
-			Icon:        "https:///",
+			ID:       1,
+			Username: "@test",
+			Name:     "Test",
+			Surname:  "Test",
+			Age:      19,
+			Gender:   0,
 		})
 		assert.Nil(t, err)
 
-		user, err := s.UserRepo.GetUser(123)
+		user, err := s.UserRepo.GetUser(1)
 		assert.Nil(t, err)
-		assert.Equal(t, "abc", user.Username)
+		assert.Equal(t, "@test", user.Username)
 	})
 
 }
